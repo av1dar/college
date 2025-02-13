939050c9d76b25eb7fa00a847f52f691e9273600
av1dar <daniik.rad.007@gmail.com>
2025-02-13 23:01:19 +0200

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+
+int main() {
+    std::string name;  
+    
+    std::cout << "Enter the name: ";  
+    std::getline(std::cin, name);  
+    
+    std::cout << "Hello, " << name << "!" << std::endl;  
+    
+    return 0;
+}
